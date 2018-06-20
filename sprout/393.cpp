2a7a8c191674e0c036752e4dd22e7f9f08f03fe1
coldEr66 <seal1000402@gmail.com>
2018-06-20 11:44:39 +0800

    AC

--- sprout/393.cpp
+++ sprout/393.cpp
@@ -51,48 +51,47 @@ template<class T> using MinHeap = priority_queue<T, vector<T>, greater<T>>;
 
 const ll MAXn=2e3+5,MAXlg=__lg(MAXn)+2;
 const ll MOD=1000000007;
-const ll INF=(ll)1e18;
+const ll INF=(ll)1e9;
 
-struct Sx{
-  ii pos;
-  ll val;
-  ll fa;
-};
-ll n,m;
-char d[MAXn][MAXn];
-ll w[MAXn][MAXn];
-ll dx[] = {-1,0,1,0};
-ll dy[] = {0,1,0,-1};
-bool chk(ll a,ll b){return a<0 || a>=n || b<0 || b>=m;}
+int n,m;
+int d[MAXn][MAXn];
+int dis[MAXn][MAXn];
+bool vis[MAXn][MAXn];
+MinHeap<pair<int,ii>> pq;
+int dx[] = {1,0,0,-1};
+int dy[] = {0,1,-1,0};
+bool chk(int a,int b){
+  return a<0 || a>=n || b<0 || b>=m;
+}
 int main(){
   IOS();
   cin>>n>>m;
-  REP(i,n)REP(j,m) cin>>d[i][j];
-  REP(i,MAXn)REP(j,MAXn) w[i][j]=INF;
-  queue<Sx> q;
-  ll s1,s2,e1,e2;
+  REP(i,n)REP(j,m){
+    char c;
+    cin>>c;
+    if(c=='#') d[i][j] = 0;
+    else d[i][j] = 1;
+  }
+  int s1,s2,e1,e2;
   cin>>s1>>s2>>e1>>e2;
   s1--,s2--,e1--,e2--;
-  Sx tmp = (Sx){(ii){s1,s2},0,-1};
-  q.push(tmp);
-  while(SZ(q)){
-    debug("hi");
-    Sx cur = q.front();
-    q.pop();
-    ll x = cur.pos.F,y = cur.pos.S;
-    REP(i,4){
-      ll tx = x + dx[i],ty = y + dy[i];
-      if(chk(tx,ty)) continue;
-      if(cur.fa==i) continue;
-      if(d[tx][ty]=='#'){
-        w[tx][ty] = cur.val;
-        q.push((Sx){(ii){tx,ty},cur.val,4-i});
-      }
-      else if(cur.val + 1 < w[tx][ty]){
-        w[tx][ty] = cur.val + 1;
-        q.push((Sx){(ii){tx,ty},cur.val+1,4-i});
+  //debug(st,dst);
+  REP(i,n)REP(j,m) dis[i][j] = INF;
+  dis[s1][s2] = d[s1][s2];
+  pq.push({dis[s1][s2],{s1,s2}});
+  REP(i,n*m){
+    ii tp = {-1,-1};
+    while(SZ(pq) && vis[tp.F = pq.top().S.F][tp.S = pq.top().S.S]) pq.pop();
+    if(tp==(ii){-1,-1}) break;
+    vis[tp.F][tp.S] = true;
+    REP(k,4){
+      int x = tp.F + dx[k],y = tp.S + dy[k];
+      if(chk(x,y)) continue;
+      if(!vis[x][y] && dis[x][y] > dis[tp.F][tp.S] + d[x][y]){
+        dis[x][y] = dis[tp.F][tp.S] + d[x][y];
+        pq.push({dis[x][y],{x,y}});
       }
     }
   }
-  cout<<w[e1][e2]<<endl;
+  cout<<dis[e1][e2]<<endl;
 }
