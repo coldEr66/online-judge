e85ec0750d92dd00133c93284085a0f5d8a11d36
colder66 <seal1000402@gmail.com>
2021-07-29 00:31:13 +0800

    20210729

--- tmp.cpp
+++ tmp.cpp
@@ -1,24 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define SZ(a) (int)a.size()
-
-
-const int MAXN = 1e3+5;
-int d[MAXN];
-
-void insertion_sort()
-int main(){
-    int n;
-    cin >> n;
-    for (int i=0;i<n;++i) cin >> d[i];
-    for (int i=1;i<n;++i) {
-        int cur = d[i], j = i-1;
-        while (cur < d[j] && j >= 0) {
-            d[j+1] = d[j];
-            j--;
-        }
-        d[j+1] = cur;
+int main() {
+    fstream file;
+    string filename = "input.txt";
+    file.open(filename, ios::in);
+    if (!file) {
+        cerr << "error!\n";
+        system("pause");
+        return 0;
     }
-    for (int i=0;i<n;++i) cout << d[i] << " \n"[i==n-1];
+
+    int tmp;
+    file >> tmp;
+    cout << tmp << endl;
 }
